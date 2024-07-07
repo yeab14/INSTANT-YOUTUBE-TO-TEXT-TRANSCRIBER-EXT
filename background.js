92cb899e0ca5a5e4b940de46207861015988040e
yeab14 <155532389+yeab14@users.noreply.github.com>
2024-07-07 06:08:37 -0700

    ip address modified

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         const videoId = extractVideoId(videoUrl);
         if (videoId) {
             console.log('Sending request to transcribe video:', videoUrl); // Log the request to transcribe the video
-            fetch(`https://fast-api-server-aoxg.onrender.com/transcribe?video_url=${encodeURIComponent(videoUrl)}`)
+            fetch(`http://185.124.109.231:9001/transcribe?video_url=${encodeURIComponent(videoUrl)}`)
                 .then(response => response.json())
                 .then(data => {
                     console.log('Received transcript data:', data); // Log the received transcript data
